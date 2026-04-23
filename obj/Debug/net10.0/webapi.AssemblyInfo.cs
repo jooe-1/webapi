a9b24bb01fb9c624d37182c8e72d94617da405f3
joo mostafa <asdewery5@gmail.com>
2026-04-24 01:54:32 +0300

    jk

--- obj/Debug/net10.0/webapi.AssemblyInfo.cs
+++ obj/Debug/net10.0/webapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a636e4b7ed1d4049fa1b56fbce2919f7cfa3e5b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16ed9342467b38edf7eb39bd20129ac85dd98915")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
