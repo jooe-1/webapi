ddbc58f1f5468122c0b0a6bc74e0b6bc12a11534
Youssef <yosefashraf360@gmail.com>
2026-04-25 14:24:57 +0300

    Some changes

--- Models/Dish.cs
+++ Models/Dish.cs
@@ -2,12 +2,10 @@ namespace webapi.Models;
 
 public class Dish
 {
-    
-        public int Id { get; init; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int AvailableBowls { get; set; } 
-        public string ImageUrl { get; set; } 
-        public string Category { get; set; } 
-    
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int AvailableBowls { get; set; }
+    public string ImageUrl { get; set; }
+    public string Category { get; set; }
 }
--- Models/Order.cs
+++ Models/Order.cs
@@ -2,11 +2,9 @@ namespace webapi.Models;
 
 public class Order
 {
-    
-        public int Id { get; set; }
-        public string CustomerName { get; set; } 
-        public string Status { get; set; } 
-        public decimal TotalPayment { get; set; }
-        public DateTime OrderDate { get; set; } = DateTime.Now;
-    
+    public int Id { get; set; }
+    public string CustomerName { get; set; }
+    public string Status { get; set; }
+    public decimal TotalPayment { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
 }
